9221e97b834ae71826f64ee6827b80edbb4404f3
SebastianLeclerc <123743404+SebastianLeclerc@users.noreply.github.com>
2024-05-29 11:42:46 +0200

    Update qos.cpp

--- qos.cpp
+++ qos.cpp
@@ -1,61 +1,60 @@
-// General
+//Tested on: 
+//          Ubuntu 20.04.6 LTS (Focal Fossa)
+//          Red Lion NT-4008-000-PN-M industrial switch with Software version: 1.0.9
+//Compile:  g++ -o qos qos.cpp -pthread
+//Run:      sudo chrt --fifo 99 ./qos
+//NOTE:     Telnet MUST be enabled on the switch (enabled via USB/Ethernet connection in web interface)
+//          Expected traffic rates values must be adapted to your traffic scenario! 
+//          Otherwise, script will send out ACLs modifying the QoS for no reason!
+         
 #include <iostream>
-// Multi-Threading
 #include <thread>
-// Event handling
 #include <condition_variable>
 #include <atomic>
-// New socketmethod
-#include <string.h>          // For memset
-//#include <unistd.h>
-// save times to file
-#include <fstream>
-// save times to file
-#include <string>
+#include <unistd.h>
 #include <cstring>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <chrono>
 #include <sstream>
 
+//Event class with mutexes
+class Event {
+    public:
+        Event() : flag(false) {}
 
-class Event
-{
-public:
-    Event() : flag(false) {}
+        void set() {
+            std::lock_guard<std::mutex> lock(mtx);
+            flag = true;
+            cv.notify_one();
+        }
 
-    void set()
-    {
-        std::lock_guard<std::mutex> lock(mtx);
-        flag = true;
-        cv.notify_one();
-    }
+        void wait() {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [this]
+                    { return flag; });
+            flag = false;
+        }
 
-    void wait()
-    {
-        std::unique_lock<std::mutex> lock(mtx);
-        cv.wait(lock, [this]
-                { return flag; });
-        flag = false;
-    }
+    private:
+        std::mutex mtx;
+        std::condition_variable cv;
+        bool flag;
+    };
 
-private:
-    std::mutex mtx;
-    std::condition_variable cv;
-    bool flag;
+//Struct for switch data
+struct SwitchData {
+    long long TxPrio7;
+    long long PTxPrio7;
 };
 
+std::atomic<bool> stopFlag(false);
 
-
-
-int openSocket(const char *switchIP)
-{
+//Open telnet socket (port 23) session to the switch
+int openSocket(const char *switchIP) {
     int sock = 0;
     struct sockaddr_in serv_addr;
     const int port = 23;
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         std::cerr << "Error creating socket: " << strerror(errno) << std::endl;
         std::cerr << "Socket creation error" << std::endl;
         return -1;
@@ -64,14 +63,12 @@ int openSocket(const char *switchIP)
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
 
-    if (inet_pton(AF_INET, switchIP, &serv_addr.sin_addr) <= 0)
-    {
+    if (inet_pton(AF_INET, switchIP, &serv_addr.sin_addr) <= 0) {
         std::cerr << "Invalid address/ Address not supported" << std::endl;
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         std::cerr << "Connection Failed" << std::endl;
         return -1;
     }
@@ -79,13 +76,12 @@ int openSocket(const char *switchIP)
     return sock;
 }
 
-void fetchStats(Event &faultEvent)
-{
+//Monitor a switch port queue and act according to expected traffic rates, sending out ACL rules
+void fetchStats(Event &faultEvent) {
     int sock = 0;
     SwitchData switchData;
 
     sock = openSocket("192.168.1.103");
-
     std::this_thread::sleep_for(std::chrono::milliseconds(400));
 
     std::string username = "admin\r\n";
@@ -107,15 +103,19 @@ void fetchStats(Event &faultEvent)
 
     std::cout << "Telnet connection established.\n";
     std::cout << "NMA is up and running...\n\n";
+
     short lowThresholdCounter = 0;
     short highThresholdCounter = 0;
-    while (true)
-    {
-        if (stopFlag.load(std::memory_order_relaxed))
-        {
+
+    while (true) {
+
+        //Close socket
+        if (stopFlag.load(std::memory_order_relaxed)) {
             close(sock);
             return;
         }
+
+        //Short query Red Lion for QoS Statistics, filter out data & save
         std::string command2 = "do sh in G 1/2 stati | i Tx Priority 7\r\n";
         send(sock, command2.c_str(), command2.length(), 0);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -125,56 +125,52 @@ void fetchStats(Event &faultEvent)
         std::string line = buffer;
         std::istringstream stream(line);
 
-        while (std::getline(stream, line))
-        {
+        //Print output
+        while (std::getline(stream, line)) {
             std::size_t pos = line.find("Tx ");
-            if (pos != std::string::npos)
-            {
+            if (pos != std::string::npos) {
                 std::string key = line.substr(pos, line.find(':', pos) - pos);
                 long long value = std::stoll(line.substr(line.find_last_of(" ") + 1));
-                if (key == "Tx Priority 7")
-                {
+                if (key == "Tx Priority 7") {
                     switchData.TxPrio7 = value;
                 }
             }
         }
 
-        if (switchData.PTxPrio7 != 0)
-        {
+        //Check if queue is > or < what we expect in a certain queue
+        if (switchData.PTxPrio7 != 0) {
             int DiffTxPrio7 = switchData.TxPrio7 - switchData.PTxPrio7;
-            if (DiffTxPrio7 > 1050)
-            {
+            if (DiffTxPrio7 > 1050) { //Change value to what your expected packets/second in a queue!
                 std::cout << "Warning " << highThresholdCounter << ": Queue No. 7 exceeded the expected rate of 1050.\n\n";
                 highThresholdCounter++;
             }
-            else if (DiffTxPrio7 < 900)
-            {
+            else if (DiffTxPrio7 < 900) { //Change value to what your expected packets/second in a queue!
                 std::cout << "Warning " << lowThresholdCounter << ": Queue No. 7 subceeded the expected rate of 1050.\n\n";
                 lowThresholdCounter++;
             }
-            else
-            {
-                if (DiffTxPrio7 != 0)
-                {
-                    if (!lowThresholdCounter < 1)
-                    {
+            else {
+                //Counters to keep track
+                if (DiffTxPrio7 != 0) {
+                    if (!lowThresholdCounter < 1) {
                         lowThresholdCounter--;
                     }
-                    if (!highThresholdCounter < 1)
-                    {
+                    if (!highThresholdCounter < 1) {
                         highThresholdCounter--;
                     }
                 }
             }
-            if (highThresholdCounter > 2)
-            {
-                std::string putIperfLowPrio = "q q 3 int G 1/1-2,8 f ipv4 p u a c 0\r\n"; //OLD version based on source MAC: q q 3 int G 1/8 s 00-d8-61-56-56-b1 a c 0
+
+            //If we are confident (3 checks) that expected traffic is too high, demote "other" UDP to Q0
+            if (highThresholdCounter > 2) {
+                //OLD ACL version based on source MAC: q q 3 int G 1/8 s 00-d8-61-56-56-b1 a c 0
+                std::string putIperfLowPrio = "q q 3 int G 1/1-2,8 f ipv4 p u a c 0\r\n";
                 send(sock, putIperfLowPrio.c_str(), putIperfLowPrio.length(), 0);
                 highThresholdCounter = 0;
                 std::cout << "Prioritized other traffic to Q0.\n\n";
             }
-            if (lowThresholdCounter > 2)
-            {
+
+            //If we are confident (3 checks) that expected traffic is too low, promote UDP with DSCP 56, demote "other" UDP
+            if (lowThresholdCounter > 2) {
                 // Remove old ACL
                 std::string removeOldACL = "no q q 3 int G 1/1-2,8 f ipv4 p u a c 0\r\n";
                 send(sock, removeOldACL.c_str(), removeOldACL.length(), 0);
@@ -194,10 +190,8 @@ void fetchStats(Event &faultEvent)
 }
 
 
-int main()
-{
-    while (true)
-    {
+int main() {
+    while (true) {
         Event fault;
         std::thread thread1(fetchStats, std::ref(fault));
         thread1.join();
